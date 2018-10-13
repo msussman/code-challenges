241bf8992ead625545430873db7dc34753cc82af
Unknown <msussman@gmail.com>
2018-10-13 18:13:29 -0400

    Step 2: productionalize the code

--- data-science/grid_search_text_feature_extraction.py
+++ data-science/grid_search_text_feature_extraction.py
@@ -1,7 +1,7 @@
 
 """
 ==========================================================
-Sample pipeline for text feature extraction and evaluation
+Pipeline for text feature extraction and evaluation
 ==========================================================
 
 The dataset used in this example is the 20 newsgroups dataset which will be
@@ -11,34 +11,7 @@ classification example.
 You can adjust the number of categories by giving their names to the dataset
 loader or setting them to None to get the 20 of them.
 
-Here is a sample output of a run on a quad-core machine::
 
-  Loading 20 newsgroups dataset for categories:
-  ['alt.atheism', 'talk.religion.misc']
-  1427 documents
-  2 categories
-
-  Performing grid search...
-  pipeline: ['vect', 'tfidf', 'clf']
-  parameters:
-  {'clf__alpha': (1.0000000000000001e-05, 9.9999999999999995e-07),
-   'clf__n_iter': (10, 50, 80),
-   'clf__penalty': ('l2', 'elasticnet'),
-   'tfidf__use_idf': (True, False),
-   'vect__max_n': (1, 2),
-   'vect__max_df': (0.5, 0.75, 1.0),
-   'vect__max_features': (None, 5000, 10000, 50000)}
-  done in 1737.030s
-
-  Best score: 0.940
-  Best parameters set:
-      clf__alpha: 9.9999999999999995e-07
-      clf__n_iter: 50
-      clf__penalty: 'elasticnet'
-      tfidf__use_idf: True
-      vect__max_n: 2
-      vect__max_df: 0.75
-      vect__max_features: 50000
 
 """
 
@@ -50,7 +23,7 @@ Here is a sample output of a run on a quad-core machine::
 from __future__ import print_function
 
 from pprint import pprint
-from time import time
+import time
 import logging
 
 from sklearn.datasets import fetch_20newsgroups
@@ -59,72 +32,116 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+from sklearn.externals import joblib
 
 print(__doc__)
 
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s')
+                    format='%(asctime.time)s %(levelname)s %(message)s')
 
 
 ###############################################################################
-# Load some categories from the training set
-categories = [
-    'alt.atheism',
-    'talk.religion.misc',
-]
-# Uncomment the following to do the analysis on all the categories
-#categories = None
+class TextFeatureExtractor():
+    def __init__(self, categories, param_grid):
+        self.categories = categories
+        self.pipeline = Pipeline([
+                                 ('vect', CountVectorizer()),
+                                 ('tfidf', TfidfTransformer()),
+                                 ('clf', SGDClassifier()),
+                                ])
+        self.param_grid = param_grid
+        self.data = None
+        self.search_results = None
+        self.best_parameters = {}
+        self.init_data()
+        self.grid_search()
+        self.grid_fit()
+        self.extract_best_parameters()
+        self.pickle_results()
 
-print("Loading 20 newsgroups dataset for categories:")
-print(categories)
 
-data = fetch_20newsgroups(subset='train', categories=categories)
-print("%d documents" % len(data.filenames))
-print("%d categories" % len(data.target_names))
-print()
+    def init_data(self):
+        '''
+        Loads 20 newsgroups training dataset for requested categories
+        '''
+        print("Loading 20 newsgroups dataset for categories:")
+        print(self.categories)
+
+        self.data = fetch_20newsgroups(subset='train', 
+                                       categories=self.categories)
+        print("%d documents" % len(self.data.filenames))
+        print("%d categories" % len(self.data.target_names))
+        print()
+        return self.data
+
+    def grid_search(self):
+        '''
+        Performs Grid Search based on passed parameters
+        '''
+        self.search_results = GridSearchCV(self.pipeline, self.param_grid, 
+                                           n_jobs=-1, verbose=1) 
+        return self.search_results
+
+    def grid_fit(self):
+        '''
+        Fits data to the Grid Search parameters
+        '''
+        print("Performing grid search...")
+        print("pipeline:", [name for name, _ in self.pipeline.steps])
+        print("parameters:")
+        pprint(self.param_grid)
+        t0 = time.time()
+        self.search_results.fit(self.data.data, self.data.target)
+        print("done in %0.3fs" % (time.time() - t0))
+        print()
+
+    def extract_best_parameters(self):
+        '''
+        extracts the best parameters from the Grid Search
+        '''
+        print("Best score: %0.3f" % self.search_results.best_score_)
+        print("Best parameters set:")
+        self.best_parameters = self.search_results.best_estimator_.get_params()
+        for param_name in sorted(self.param_grid.keys()):
+            print("\t%s: %r" % (param_name, self.best_parameters[param_name]))
+        return self.best_parameters
+    
+    def pickle_results(self):
+        '''
+        Pickles the results into timestamped file 
+        '''
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        pickle_name = 'grid_search_parameters_{}.pkl'.format(timestr)
+        joblib.dump(self.best_parameters, pickle_name, compress = 1)
+
 
 ###############################################################################
+
 # define a pipeline combining a text feature extractor with a simple
 # classifier
-pipeline = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('clf', SGDClassifier()),
-])
 
-# uncommenting more parameters will give better exploring power but will
-# increase processing time in a combinatorial way
-parameters = {
-    'vect__max_df': (0.5, 0.75, 1.0),
-    #'vect__max_features': (None, 5000, 10000, 50000),
-    'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
-    #'tfidf__use_idf': (True, False),
-    #'tfidf__norm': ('l1', 'l2'),
-    'clf__alpha': (0.00001, 0.000001),
-    'clf__penalty': ('l2', 'elasticnet'),
-    #'clf__n_iter': (10, 50, 80),
-}
+def main():
+
+    # Define Categories to pull newsfeed training data for, None will pull all 
+    categories = ['alt.atheism', 'talk.religion.misc']
+
+    # Pass the grid parameters to use in the search
+    param_grid = {
+        'vect__max_df': (0.5, 0.75, 1.0),
+        #'vect__max_features': (None, 5000, 10000, 50000),
+        'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+        #'tfidf__use_idf': (True, False),
+        #'tfidf__norm': ('l1', 'l2'),
+        'clf__alpha': (0.00001, 0.000001),
+        'clf__penalty': ('l2', 'elasticnet'),
+        #'clf__n_iter': (10, 50, 80),
+    }
+    # Run text feature extractor
+    TextFeatureExtractor(categories, param_grid)
+
 
 if __name__ == "__main__":
-    # multiprocessing requires the fork to happen in a __main__ protected
-    # block
 
-    # find the best parameters for both the feature extraction and the
-    # classifier
-    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1)
+    main()
 
-    print("Performing grid search...")
-    print("pipeline:", [name for name, _ in pipeline.steps])
-    print("parameters:")
-    pprint(parameters)
-    t0 = time()
-    grid_search.fit(data.data, data.target)
-    print("done in %0.3fs" % (time() - t0))
-    print()
-
-    print("Best score: %0.3f" % grid_search.best_score_)
-    print("Best parameters set:")
-    best_parameters = grid_search.best_estimator_.get_params()
-    for param_name in sorted(parameters.keys()):
-        print("\t%s: %r" % (param_name, best_parameters[param_name]))
